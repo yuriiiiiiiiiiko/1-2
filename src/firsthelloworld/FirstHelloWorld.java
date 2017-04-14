bf3ad480cec99a081f64753187446f355b1ec57a
g15906fy <g15906fy@s30446.ACTSUDA>
2017-04-14 11:58:30 +0900

    日本語を追加しました

--- src/firsthelloworld/FirstHelloWorld.java
+++ src/firsthelloworld/FirstHelloWorld.java
@@ -17,6 +17,7 @@ public class FirstHelloWorld {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello world");
+        System.out.println("こんにちは");
     }
     
 }
